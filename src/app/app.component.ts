baff6fbb6cd795225f1835c685f85245b4f0b981
Boris <boris.chen@kdis.com>
2017-10-25 16:14:53 +1100

    bug fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ declare var window;
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, apiai) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
