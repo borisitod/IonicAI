f1141fbf0e0e338c227bb49300e2cb26cd5b131e
Boris <boris.chen@kdis.com>
2017-10-25 16:28:20 +1100

    sending and receiving message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var window;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,16 +10,36 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   messages: any[] = [];
+  text: string = "";
   constructor(public navCtrl: NavController) {
       this.messages.push({
           text: 'Hi, how can I help you',
           sender: 'api'
       })
+  }
+
+  sendText(){
+
+      let message = this.text;
 
       this.messages.push({
-          text: 'Hello',
-          sender: 'me'
+        text: message,
+        sender: 'me'
       })
+
+      this.text = "";
+
+      window['ApiAIPlugin'].requestText({
+          query: message
+      }, (response)=>{
+          this.messages.push({
+              text: response.result.fullfilment.speech,
+              sender: "api"
+          })
+      }, (error)=>{
+          alert(JSON.stringify(error))
+      },)
   }
+
 
 }
