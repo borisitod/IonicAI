5f0580c9b0e60eeecd71f3148c2cf36e6b8e5cfa
Boris <boris.chen@kdis.com>
2017-10-25 15:46:26 +1100

    install cordova api ai plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare var window;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, apiai) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    window['ApiAIPlugin'].init(
+        {
+            clientAccessToken: "ad585e9a3f714b559fabd009f7df7c5c", // insert your client access key here
+            lang: "en" // set lang tag from list of supported languages
+        },
+        function(result) {
+          alert(result)
+
+        },
+        function(error) { alert(error) }
+    );
   }
 }
 
